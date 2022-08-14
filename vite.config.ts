aef4fe0b13d391128c1663c5c7abca0e0ce9c3e3
Mayur Panchal <mayur.panchal@deepintent.com>
2022-08-14 11:09:09 +0530

    chore: :wrench: add library related changes
    various configs for publishing lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts"
+import path from "node:path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      /**
+       * All the public components are stored in src/components and exported from index.ts
+       */
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "MyLib",
+      formats: ["es", "umd"],
+      fileName: (format) => `my-lib.${format}.js`
+    },
+    rollupOptions: {
+      /**
+       * Externalize all the dependencies that you don't want to bundle into the library.
+       */
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          classnames: "classNames"
+        }
+      }
+    }
+  }
 })
